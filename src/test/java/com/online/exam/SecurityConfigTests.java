8436288c77314d068fa6087f8db69af78412264d
aravidhan2307 <720823106009@hit.edu.in>
2026-07-16 11:27:32 +0530

    add

--- src/test/java/com/online/exam/SecurityConfigTests.java
+++ src/test/java/com/online/exam/SecurityConfigTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @SpringBootTest
 @AutoConfigureMockMvc
